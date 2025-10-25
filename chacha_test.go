5b6c9f5782d93eacbf92667a1254efebefdd4242
FrostyKitten02 <alen.fridau02@gmail.com>
2025-10-25 22:48:51 +0200

    test

--- chacha_test.go
+++ chacha_test.go
@@ -16,19 +16,22 @@ func TestTime(t *testing.T) {
 	key := internal.Key{1, 2, 3, 4, 5, 6, 7, 8}
 	nonce := internal.Nonce{1, 2, 3}
 
-	//internal.EncryptParallelFile(key, 0, nonce, "bigger_boy.mkv", "bigger_boy22.mkv")
-	file, _ := os.ReadFile("bigger_boy.mkv")
+	file, _ := os.ReadFile("test.mkv")
+	out := encrypt(key, 0, nonce, file, internal.EncryptParallel, "parallel")
+	os.WriteFile("test.mkv.enc", out, 0777)
 
-	encrypt(key, 0, nonce, file, internal.EncryptParallel, "parallel")
 	encrypt(key, 0, nonce, file, internal.Encrypt, "sequential")
+
 }
 
-func encrypt(key internal.Key, counter uint32, nonce internal.Nonce, data []byte, e func(internal.Key, uint32, internal.Nonce, []byte) []byte, name string) {
+func encrypt(key internal.Key, counter uint32, nonce internal.Nonce, data []byte, e func(internal.Key, uint32, internal.Nonce, []byte) []byte, name string) []byte {
 	size := len(data)
 	megaSize := float64(size) / 1_000_000
 	start := time.Now().UnixNano()
-	e(key, counter, nonce, data)
+	res := e(key, counter, nonce, data)
 	totalTime := float64(time.Now().UnixNano()-start) / 1_000_000_000
 	fmt.Println("Encrypted " + name + " in: " + strconv.FormatFloat(totalTime, 'f', 3, 64) + "s")
 	fmt.Println("Encrypt " + name + ": " + strconv.FormatFloat(megaSize/totalTime, 'f', 3, 64) + "MB/s")
+
+	return res
 }
